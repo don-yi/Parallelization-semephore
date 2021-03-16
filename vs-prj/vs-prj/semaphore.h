a51113444b62eaa8aeea42ce754adfe19f9b646f
don <97c8caf0-dc2e-4bdc-aaeb-91ce444be156@anonaddy.me>
2021-03-16 12:35:49 +0900

    setup

---
+++ vs-prj/vs-prj/semaphore.h
@@ -0,0 +1,23 @@
+class Semaphore
+{
+
+public:
+
+	Semaphore(int maxThr) : maxThr(maxThr) {};
+
+	void wait() {
+	  
+	}
+
+	void signal() {
+	  
+	}
+
+
+private:
+
+	int maxThr = -1;
+	int numThr = 0;
+
+};
+
